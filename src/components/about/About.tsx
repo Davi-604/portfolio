15cc11ba69d93df4268ea885d028a9021ee58109
Davi-604 <davimedeiroscv100@gmail.com>
2025-12-16 22:55:35 -0300

    style: atualiza o texto da sessão "Sobre mim"

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,9 +1,9 @@
-import { LuUserSearch } from 'react-icons/lu';
-import { DefaultSectionTittle } from '../default/DefaultSectionTittle';
-import { motion } from 'framer-motion';
 import { fadeInUp, slowFadeInLeft } from '@/animations/fadeIn';
 import { useSectionStore } from '@/stores/useSectionStore';
+import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
+import { LuUserSearch } from 'react-icons/lu';
+import { DefaultSectionTittle } from '../default/DefaultSectionTittle';
 
 export const About = () => {
     const { currentSection, setCurrentSection } = useSectionStore();
@@ -31,26 +31,30 @@ export const About = () => {
                 variants={slowFadeInLeft}
                 className="mt-16 text-lg text-muted-foreground inline-block lg:mt-20 lg:max-w-[1100px] lg:text-xl"
             >
-                Olá, eu me chamo Davi Carvalho e sou um programador Full-stack que gosta
-                muito do que faz. Tenho 18 anos e estudo programação há cerca de 2 anos.
-                Desde então, não consigo imaginar outro objetivo de carreira que não seja
-                me tornar o melhor programador possível.
+                Me chamo Davi Carvalho e sou um programador Full-stack que está sempre em busca de entregar as 
+                melhores soluções, seja solucionando problemas de forma inteligente ou criando implementações
+                inovadoras.
+
                 <br />
                 <br />
-                Ao longo da minha jornada como estudante, aprendi muito sobre a
-                responsabilidade que devo ter com o meu trabalho e o verdadeiro propósito
-                de tudo isso: <b>resolver problemas</b>.
+
+                Com uma trajetória marcada por diversas experiências dentro do desenvolvimento de software,
+                adquiri uma visão clara sobre a responsabilidade do meu trabalho.
+                Aprendi que o verdadeiro propósito do desenvolvimento não é apenas "fazer funcionar", mas sim entregar sistemas
+                performáticos, seguros e que resolvam problemas reais de forma definitiva e escalável.
+
                 <br />
                 <br />
+
                 Com isso em mente, busco constantemente aprender novas formas de aprimorar
-                minhas habilidades e expandir meu conhecimento na área. Meu objetivo é me
-                capacitar para resolver cada vez mais desafios de maneira eficiente e
-                criativa.
+                minhas habilidades e expandir meu conhecimento na área. Assim meu objetivo se encontra em me
+                melhorar para resolver cada vez mais desafios de maneira eficiente e criativa.
+
                 <br />
                 <br />
                 Por isso, me concentro em agregar valor ao ambiente em que estou inserido
                 e em aproveitar ao máximo o que esse ambiente tem a oferecer. Dessa forma,
-                espero ser sempre útil e produtivo, contribuindo para o sucesso das
+                busco ser sempre útil e produtivo, contribuindo para o sucesso das
                 pessoas e dos projetos a minha volta.
             </motion.div>
         </div>
